0d24588621d2e6c471086c5a20dcec54a5b4a7f0
chenjiefei <nlpx_dc@hotmail.com>
2019-05-30 14:15:02 +0800

    draft export user xls with data

--- src/main/java/io/cjf/testoffice/controller/UserController.java
+++ src/main/java/io/cjf/testoffice/controller/UserController.java
@@ -19,6 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -37,19 +38,63 @@ public class UserController {
     }
 
     @GetMapping(value = "/exportXls", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    public byte[] exportXls(HttpServletResponse response) throws IOException {
+    public byte[] exportXls(HttpServletResponse response) throws IOException, IllegalAccessException {
         List<User> users = userMapper.selectAll();
 
         Workbook workbook = new HSSFWorkbook();
         Sheet sheet = workbook.createSheet("users");
 
-        Row row = sheet.createRow(0);
-        Field[] declaredFields = User.class.getDeclaredFields();
+        Field[] declaredFields;
+        Row row;
+
+        row = sheet.createRow(0);
+        declaredFields = User.class.getDeclaredFields();
         for (int i = 0; i < declaredFields.length; i++) {
             Field field = declaredFields[i];
             String name = field.getName();
             Cell cell = row.createCell(i);
             cell.setCellValue(name);
+        }
+
+        for (int i = 0; i < users.size(); i++) {
+            User user = users.get(i);
+            row = sheet.createRow(i + 1);
+            declaredFields = user.getClass().getDeclaredFields();
+            for (int j = 0; j < declaredFields.length; j++) {
+                Field field = declaredFields[j];
+                Cell cell = row.createCell(j);
+
+                field.setAccessible(true);
+                Object value = field.get(user);
+                if (value instanceof String) {
+                    cell.setCellValue((String) value);
+                }
+                if (value instanceof Long){
+                    cell.setCellValue((Long) value);
+                }
+                if (value instanceof Integer){
+                    cell.setCellValue((Integer) value);
+                }
+                if (value instanceof Short){
+                    cell.setCellValue((Short) value);
+                }
+                if (value instanceof Byte){
+                    cell.setCellValue((Byte) value);
+                }
+                if (value instanceof Double){
+                    cell.setCellValue((Double) value);
+                }
+                if (value instanceof Float){
+                    cell.setCellValue((Float) value);
+                }
+                if (value instanceof Boolean) {
+                    cell.setCellValue((Boolean) value);
+                }
+                if (value instanceof Date) {
+                    Date date = (Date) value;
+                    cell.setCellValue(date.toString());
+                }
+            }
         }
 
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
