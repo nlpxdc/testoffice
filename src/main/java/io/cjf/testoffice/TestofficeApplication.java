1babfa53af11dbff449f2ece832696e762730054
chenjiefei <nlpx_dc@hotmail.com>
2019-05-30 09:34:59 +0800

    first init

---
+++ src/main/java/io/cjf/testoffice/TestofficeApplication.java
@@ -0,0 +1,13 @@
+package io.cjf.testoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestofficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestofficeApplication.class, args);
+    }
+
+}
