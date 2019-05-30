ac174b6ddc8742b2d27d1e47fc602bf2d0daeeb0
chenjiefei <nlpx_dc@hotmail.com>
2019-05-30 14:38:55 +0800

    add export xlsx

--- src/main/java/io/cjf/testoffice/controller/UserController.java
+++ src/main/java/io/cjf/testoffice/controller/UserController.java
@@ -8,9 +8,9 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -115,6 +115,88 @@ public class UserController {
         baos.close();
 
         String filename = UUID.randomUUID().toString() + ".xls";
+        response.setHeader("Content-Disposition", "attachment; filename=" + filename);
+
+        return data;
+    }
+
+    @GetMapping(value = "/exportXlsx", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public byte[] exportXlsx(HttpServletResponse response) throws IOException, IllegalAccessException {
+        List<User> users = userMapper.selectAll();
+
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("users");
+
+        Field[] declaredFields;
+        Row row;
+
+        row = sheet.createRow(0);
+        declaredFields = User.class.getDeclaredFields();
+        for (int i = 0; i < declaredFields.length; i++) {
+            Field field = declaredFields[i];
+            String name = field.getName();
+            Cell cell = row.createCell(i);
+            cell.setCellValue(name);
+        }
+
+        for (int i = 0; i < users.size(); i++) {
+            User user = users.get(i);
+            row = sheet.createRow(i + 1);
+            declaredFields = user.getClass().getDeclaredFields();
+            for (int j = 0; j < declaredFields.length; j++) {
+                Field field = declaredFields[j];
+                Cell cell = row.createCell(j);
+
+                field.setAccessible(true);
+                Object value = field.get(user);
+                if (value instanceof String) {
+                    cell.setCellValue((String) value);
+                }
+                if (value instanceof Long) {
+                    cell.setCellValue((Long) value);
+                }
+                if (value instanceof Integer) {
+                    cell.setCellValue((Integer) value);
+                }
+                if (value instanceof Short) {
+                    cell.setCellValue((Short) value);
+                }
+                if (value instanceof Byte) {
+                    cell.setCellValue((Byte) value);
+                }
+                if (value instanceof Double) {
+                    cell.setCellValue((Double) value);
+                }
+                if (value instanceof Float) {
+                    cell.setCellValue((Float) value);
+                }
+                if (value instanceof Boolean) {
+                    cell.setCellValue((Boolean) value);
+                }
+                if (value instanceof Date) {
+                    Date date = (Date) value;
+                    cell.setCellValue(date.toString());
+                }
+
+                String name = field.getName();
+                if (name.equals("gender")) {
+                    byte numericCellValue = (byte) cell.getNumericCellValue();
+                    cell.setCellValue(String.valueOf(Gender.values()[numericCellValue]));
+                }
+                if (name.equals("enabled")){
+                    boolean enabled = cell.getBooleanCellValue();
+                    cell.setCellValue(enabled ? "启用" : "禁用");
+                }
+            }
+        }
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        workbook.write(baos);
+        workbook.close();
+        byte[] data = baos.toByteArray();
+        baos.close();
+
+        String filename = UUID.randomUUID().toString() + ".xlsx";
         response.setHeader("Content-Disposition", "attachment; filename=" + filename);
 
         return data;
